9895039c3e09bf92280bb63980a931b852bdff64
romain <romain.casagrande.pro@gmail.com>
2023-09-05 13:37:38 +0200

    add_layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import s from "./style.module.css";
+import "./global.css";
+
 export function App(){
-    return <div>Hel</div>
+    return (
+    <div className={s.main_container}>
+        <div className={s.header}>header</div>
+        <div className={s.show_tv_detail}>detail</div>
+        <div className={s.recommentation}>recommentation</div>
+    </div>
+    )
 }
